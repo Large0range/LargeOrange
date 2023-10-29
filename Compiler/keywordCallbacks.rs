a21fa0622702f087def72ff3204aebe6d0d57cd1
Alex Roy <Large0range@github.com>
2023-10-28 20:08:06 -0400

    abstracted the function callbacks for instructions into their own file

--- Compiler/keywordCallbacks.rs
+++ Compiler/keywordCallbacks.rs
@@ -1,3 +1,52 @@
-pub fn los() {
-    println!("test");
+use std::collections::HashMap;
+
+pub fn remember(args: &[&str], Hash: &mut HashMap<String, String>) { // function for the remember instruction
+    Hash.insert(args[0].to_string(), args[1].to_string()); // insert variable name and value into hash map 
+}
+
+
+pub fn say(args: &[&str], _Hash: &mut HashMap<String, String>) { // function for telling the computer to output something
+    let joined = args.join(" "); // join the following arguments passed to the say instruction
+    
+    
+    println!("{}", joined); //output the final string
+}
+
+pub fn add(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+    Hash.insert(variableName.to_string(), (int1+int2).to_string());
+}
+
+pub fn sub(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1-int2).to_string());
+}
+
+pub fn mul(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1*int2).to_string());
+}
+
+pub fn div(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1/int2).to_string());    
 }
--- Compiler/main.rs
+++ Compiler/main.rs
@@ -9,65 +9,16 @@ use regex::Regex;
 
 //Regex::new(r"(\[\w+\])").unwrap(); -> Regex matcher for the variables
 
-fn remember(args: &[&str], Hash: &mut HashMap<String, String>) { // function for the remember instruction
-    Hash.insert(args[0].to_string(), args[1].to_string()); // insert variable name and value into hash map 
-}
 
-
-fn say(args: &[&str], _Hash: &mut HashMap<String, String>) { // function for telling the computer to output something
-    let joined = args.join(" "); // join the following arguments passed to the say instruction
-    
-    
-    println!("{}", joined); //output the final string
-}
-
-fn add(args: &[&str], Hash: &mut HashMap<String, String>) {
-    let variableName = args[0]; // get the variable to store the output in 
-    
-    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
-    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
-
-    Hash.insert(variableName.to_string(), (int1+int2).to_string());
-}
-
-fn sub(args: &[&str], Hash: &mut HashMap<String, String>) {
-    let variableName = args[0]; // get the variable to store the output in 
-    
-    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
-    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
-
-
-    Hash.insert(variableName.to_string(), (int1-int2).to_string());
-}
-
-fn mul(args: &[&str], Hash: &mut HashMap<String, String>) {
-    let variableName = args[0]; // get the variable to store the output in 
-    
-    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
-    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
-
-
-    Hash.insert(variableName.to_string(), (int1*int2).to_string());
-}
-
-fn div(args: &[&str], Hash: &mut HashMap<String, String>) {
-    let variableName = args[0]; // get the variable to store the output in 
-    
-    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
-    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
-
-
-    Hash.insert(variableName.to_string(), (int1/int2).to_string());    
-}
 
 fn CreateHashMap() -> HashMap<String, fn(&[&str], &mut HashMap<String, String>)> { //create the hash map for instruction to function saving
     let TokenHashMap: HashMap<String, fn(&[&str], &mut HashMap<String, String>)> = HashMap::from([
-        ("remember".to_string(), remember as fn(&[&str], &mut HashMap<String, String>)), // remember instruction
-        ("say".to_string(), say as fn (&[&str], &mut HashMap<String, String>)),          // say instruction
-        ("add".to_string(), add as fn(&[&str], &mut HashMap<String, String>)), // add instruction
-        ("sub".to_string(), sub as fn(&[&str], &mut HashMap<String, String>)), // sub instruction
-        ("mul".to_string(), mul as fn(&[&str], &mut HashMap<String, String>)), // mul instruction
-        ("div".to_string(), div as fn(&[&str], &mut HashMap<String, String>)), // div instruction
+        ("remember".to_string(), keywordCallbacks::remember as fn(&[&str], &mut HashMap<String, String>)), // remember instruction
+        ("say".to_string(), keywordCallbacks::say as fn (&[&str], &mut HashMap<String, String>)),          // say instruction
+        ("add".to_string(), keywordCallbacks::add as fn(&[&str], &mut HashMap<String, String>)), // add instruction
+        ("sub".to_string(), keywordCallbacks::sub as fn(&[&str], &mut HashMap<String, String>)), // sub instruction
+        ("mul".to_string(), keywordCallbacks::mul as fn(&[&str], &mut HashMap<String, String>)), // mul instruction
+        ("div".to_string(), keywordCallbacks::div as fn(&[&str], &mut HashMap<String, String>)), // div instruction
     ]);
 
     return TokenHashMap;
