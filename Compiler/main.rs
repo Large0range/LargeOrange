de88001767a86bab467e5f45646a4a307075f79b
Alex Roy <Large0range@github.com>
2023-10-28 20:02:24 -0400

    Added addition, subtraction, multiplication, and division instructions and updated for one keyword variable declaration

--- Compiler/main.rs
+++ Compiler/main.rs
@@ -13,37 +13,61 @@ fn remember(args: &[&str], Hash: &mut HashMap<String, String>) { // function for
     Hash.insert(args[0].to_string(), args[1].to_string()); // insert variable name and value into hash map 
 }
 
-fn change(args: &[&str], Hash: &mut HashMap<String, String>) { // function for the change instruction
-    *Hash.get_mut(args[0]).unwrap() = args[1].to_string(); // change the hash table value to args[1]
+
+fn say(args: &[&str], _Hash: &mut HashMap<String, String>) { // function for telling the computer to output something
+    let joined = args.join(" "); // join the following arguments passed to the say instruction
+    
+    
+    println!("{}", joined); //output the final string
 }
 
-
-fn say(args: &[&str], Hash: &mut HashMap<String, String>) { // function for telling the computer to output something
-    let mut joined = args.join(" "); // join the following arguments passed to the say instruction
+fn add(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
     
-    let RegexMatch = Regex::new(r"(\[\w+\])").unwrap(); // create a regex matcher for any word within square brackets
-    let iter = RegexMatch.find_iter(&joined); // get all instances of this
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
 
-    let mut fixed = "".to_string(); // create a dummy string for scoping reasons
+    Hash.insert(variableName.to_string(), (int1+int2).to_string());
+}
 
-    //BLOCK ALLOWS FOR VARIABLE OUTPUT
-    for l in iter { // iterate over each instance of a word in square brackets
-        let variableReplacement = l.as_str(); // get the actual string found
-        let variableName = variableReplacement.replace("[", "").replace("]", ""); // purge the square brackets from the string
-        match Hash.get(&variableName) { // hashmap lookup for the variable name inside the square brackets
-            Some(S) => fixed = joined.replace(&variableReplacement, S), // set dummy string fixed to join modified with the variable value
-            None => (), 
-        }
-    } 
+fn sub(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
     
-    println!("{}", &fixed); //output the final string
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1-int2).to_string());
+}
+
+fn mul(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1*int2).to_string());
+}
+
+fn div(args: &[&str], Hash: &mut HashMap<String, String>) {
+    let variableName = args[0]; // get the variable to store the output in 
+    
+    let int1 = args[1].parse::<i32>().unwrap(); // get the first number, parse and unwrap both
+    let int2 = args[2].parse::<i32>().unwrap(); // get the second number
+
+
+    Hash.insert(variableName.to_string(), (int1/int2).to_string());    
 }
 
 fn CreateHashMap() -> HashMap<String, fn(&[&str], &mut HashMap<String, String>)> { //create the hash map for instruction to function saving
     let TokenHashMap: HashMap<String, fn(&[&str], &mut HashMap<String, String>)> = HashMap::from([
         ("remember".to_string(), remember as fn(&[&str], &mut HashMap<String, String>)), // remember instruction
         ("say".to_string(), say as fn (&[&str], &mut HashMap<String, String>)),          // say instruction
-        ("change".to_string(), change as fn(&[&str], &mut HashMap<String, String>)),     // change instruction
+        ("add".to_string(), add as fn(&[&str], &mut HashMap<String, String>)), // add instruction
+        ("sub".to_string(), sub as fn(&[&str], &mut HashMap<String, String>)), // sub instruction
+        ("mul".to_string(), mul as fn(&[&str], &mut HashMap<String, String>)), // mul instruction
+        ("div".to_string(), div as fn(&[&str], &mut HashMap<String, String>)), // div instruction
     ]);
 
     return TokenHashMap;
@@ -60,16 +84,35 @@ fn main() {
     let mut VariableHashMap: HashMap<String, String> = HashMap::new();
 
 
-    let clean = sourceCode.replace("\n", ""); // purge new lines
+    let clean = sourceCode.replace("\n", "").replace("\r", ""); // purge new lines, and the fucking carriage returns cuz windows is just retarded
     let splitCode = clean.split(";"); // split the code by ;
 
-    for line in splitCode { // loop through each line in the source code
+    let RegexMatch = Regex::new(r"(\[\w+\])").unwrap(); // Create the regex matcher for the iterator that finds variable names in the lines
+
+    for mut line in splitCode { // loop through each line in the source code
         if line.len() == 0 { continue; } //if the line is empty then just continue
         if line.starts_with("//") { continue; } // LINE ACCOUNTS FOR COMMENTS
 
+
+        let iter = RegexMatch.find_iter(&line); // get all instances of [VARIABLE_NAME]
+    
+        let mut fixed = "".to_string(); // create a dummy string
+
+        //Loop over every found variable name
+        for l in iter { // iterate over each instance of a word in square brackets
+            let variableReplacement = l.as_str(); // get the actual string found
+            let variableName = variableReplacement.replace("[", "").replace("]", ""); // purge the square brackets from the string
+            match VariableHashMap.get(&variableName) { // hashmap lookup for the variable name inside the square brackets
+                Some(S) => fixed = line.replace(&variableReplacement, S), // set dummy string fixed to join modified with the variable value
+                None => (), // handle none
+            }
+        } 
+
+        if !fixed.is_empty() { line = &fixed; } //avoid having a random empty string that then gets parsed 
+
         let mut words = line.split(" "); // split the code by spaces
         
-        let Some(S) = words.nth(0) else { return () }; // little rust magic, just getting the &str in S, None must be handled which is the reason for else return ()
+        let Some(S) = words.nth(0) else { return () }; // Get the string of the first element of the split line 
 
         match TokenHashMap.get(&S.to_string()) {  //match the first word to a keyword in the hashmap
             Some(t) => {
--- Compiler/test.em
+++ Compiler/test.em
@@ -1,4 +1,22 @@
 remember greeting hi;
-change greeting no;
+remember greeting no;
+
+add my_add 1 1;
+add my_add [my_add] 1;
+
+sub my_sub 2 1;
+
+mul my_mul 3 1;
+
+div my_div 4 2;
+
+say [my_add];
+say [my_sub];
+say [my_mul];
+say [my_div];
+
+say [greeting];
+
+remember greeting test;
 say [greeting];
 //test comment
